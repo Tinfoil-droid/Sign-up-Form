724c4c49ca6213f6aa163c0606e4bb364c0974f9
Tinfoil-droid <137796947+Tinfoil-droid@users.noreply.github.com>
2023-09-29 16:02:07 +0100

    improved password checker and adjusted vh

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-  let password = document.querySelector("#password").value;
-  let confirmPassword = document.querySelector("#confirmPassword").value;
-
+  // Get references to the form and error element
+  let form = document.querySelector("form");
   let error = document.querySelector("#error");
 
-  if (password == "" && confirmPassword == "") {
-    error.innerHTML = "* Passwords do not match";
-  } else if (password === confirmPassword) {
-    error.innerHTML = "";
-  }
-  console.log("loaded");
+  // Attach an event listener to the form's submit event
+  form.addEventListener("submit", function (event) {
+    // Prevent the form from submitting by default
+    event.preventDefault();
+
+    // Get the values of the password fields
+    let password = document.querySelector("#password").value;
+    let confirmPassword = document.querySelector("#confirmPassword").value;
+
+    // Check if the passwords match
+    if (password !== confirmPassword) {
+      error.innerHTML = "* Passwords do not match";
+    } else {
+      // Clear the error message if passwords match
+      error.innerHTML = "";
+      // Submit the form if the passwords match (you might want to add this)
+      form.submit();
+    }
+  });
 });
